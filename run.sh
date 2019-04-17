5380f6f5977b890abff81a935c269857af2286a6
Yucao42 <yc3390@nyu.edu>
2019-04-16 23:00:03 -0400

    delete big file

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 
 #wget https://people.eecs.berkeley.edu/~kanazawa/cachedir/hmr/models.tar.gz && tar -xf models.tar.gz
-python -m demo --img_path data/coco1.png
-#python -m demo --img_path data/im1954.jpg
+#python -m demo --img_path data/coco1.png
+python -m demo --img_path data/im1954.jpg
